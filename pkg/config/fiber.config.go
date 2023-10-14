23ee6d4931786e7b47ce3002c490c7b22998e10b
Ahmad940 <ahmadmuhammadmak5@gmail.com>
2023-10-14 08:42:19 +0100

    Update: Implemented auth 🔥🚀

--- pkg/config/fiber.config.go
+++ pkg/config/fiber.config.go
@@ -1,14 +1,14 @@
 package config
 
-import (
-	"github.com/goccy/go-json"
-	"github.com/gofiber/fiber/v2"
-)
+import "github.com/gofiber/fiber/v2"
+
+// "github.com/goccy/go-json"
+// "github.com/gofiber/fiber/v2"
 
 func FiberConfig() fiber.Config {
 	return fiber.Config{
 		// UnescapePath: true,
-		JSONEncoder: json.Marshal,
-		JSONDecoder: json.Unmarshal,
+		// JSONEncoder: json.Marshal,
+		// JSONDecoder: json.Unmarshal,
 	}
 }
