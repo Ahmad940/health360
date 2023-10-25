e1d8f5c4792787e0089cce506e915733b5285927
Ahmad940 <ahmadmuhammadmak5@gmail.com>
2023-10-25 10:56:57 +0100

    Bug fixed: fixed authentication issue

--- app/handler/ussd.handler.go
+++ app/handler/ussd.handler.go
@@ -11,12 +11,12 @@ func StartUSSD(ctx *fiber.Ctx) error {
 	sessionId := ctx.Params("sessionId")
 	serviceCode := ctx.Params("serviceCode")
 	phoneNumber := ctx.Params("phoneNumber")
-
-	fmt.Println("Session Param 1", ctx.Params("sessionId"))
-	fmt.Println("Session Query 2", ctx.Query("sessionId"))
-	fmt.Println("Session GET 3", ctx.Get("sessionId"))
-
 	text := ctx.Params("text")
+
+	fmt.Println("Session Param 1", ctx.Params("sessionId"), ctx.Params("text"))
+	fmt.Println("Session Query 2", ctx.Query("sessionId"), ctx.Query("text"))
+	fmt.Println("Session GET 3", ctx.Get("sessionId"), ctx.Get("text"))
+
 	res := service.StartUSSD(sessionId, serviceCode, phoneNumber, text)
 
 	ctx.Set("Content-Type", "text/plain")
