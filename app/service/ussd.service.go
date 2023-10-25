67c0e989bc69e749a80bea5b27ac7b70e09c6529
Ahmad940 <ahmadmuhammadmak5@gmail.com>
2023-10-25 12:00:45 +0100

    Bug fixed: fixed authentication issue

--- app/service/ussd.service.go
+++ app/service/ussd.service.go
@@ -12,7 +12,7 @@ func StartUSSD(sessionId, serviceCode, phoneNumber, text string) string {
 	// return "Hello World"
 	if text == "" {
 		// This is the first request. Note how we start the response with CON
-		return `CON Welcome to health360 USSD service, please select an option
+		return `CON Welcome to health360 USSD service, please select an option below:
 		1. Book appointment
 		2. Cancel appointment`
 	} else if text == "1" {
@@ -37,7 +37,8 @@ func StartUSSD(sessionId, serviceCode, phoneNumber, text string) string {
 		1. See Details
 		2. Book (₦1000)`
 	} else if isValid, _ := regexp.MatchString(`^1\*[1-6]\*[1-2]\*[1-2]$`, text); isValid { // 1*[1-5]*[1-2]
-		return `CON Consultant detail: Dr Junaid is an Oxford graduate in cardiology, he has been practicing for 15 years and has dozen record to his belt.
+		return `CON Consultant detail: 
+		Dr Junaid is an Oxford graduate in cardiology, he has been practicing for 15 years and has dozen record to his belt.
 		1. ₦Book`
 	} else if isValid, _ := regexp.MatchString(`^1\*[1-6]\*[1-2]\*[1-2]\*[1-2]$`, text); isValid { // 1*[1-5]*[1-2]
 		return `END Not available at the moment, Coming soon`
