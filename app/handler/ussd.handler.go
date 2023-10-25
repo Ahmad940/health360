a6bb8aab284e7d7139e4081190d96b5d0ba37c03
Ahmad940 <ahmadmuhammadmak5@gmail.com>
2023-10-25 10:47:59 +0100

    Bug fixed: fixed authentication issue

--- app/handler/ussd.handler.go
+++ app/handler/ussd.handler.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"fmt"
+
 	"github.com/Ahmad940/health360/app/service"
 	"github.com/gofiber/fiber/v2"
 )
@@ -9,8 +11,14 @@ func StartUSSD(ctx *fiber.Ctx) error {
 	sessionId := ctx.Params("sessionId")
 	serviceCode := ctx.Params("serviceCode")
 	phoneNumber := ctx.Params("phoneNumber")
+
+	fmt.Println("Session 1", ctx.Params("sessionId"))
+	fmt.Println("Session 2", ctx.Query("sessionId"))
+	fmt.Println("Session 3", ctx.Get("sessionId"))
+
 	text := ctx.Params("text")
 	res := service.StartUSSD(sessionId, serviceCode, phoneNumber, text)
 
+	ctx.Set("Content-Type", "text/plain")
 	return ctx.Send([]byte(res))
 }
--- app/service/ussd.service.go
+++ app/service/ussd.service.go
@@ -1,6 +1,18 @@
 package service
 
 func StartUSSD(sessionId, serviceCode, phoneNumber, text string) string {
+	if text == "" {
+		// This is the first request. Note how we start the response with CON
+		return `CON What would you like to check
+        1. My account
+        2. My phone number`
+	} else if text == "1" {
+		// Business logic for first level response
+		return `CON Choose account information you want to view
+        1. Account number`
+	} else if text == "1*1" {
+		return "Oi Oi"
+	}
 
 	return "Hello World"
 }
