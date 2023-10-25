69599965f7f83cd25862750b44ebf50ab3a90ed5
Ahmad940 <ahmadmuhammadmak5@gmail.com>
2023-10-25 11:40:23 +0100

    Bug fixed: fixed authentication issue

--- app/handler/ussd.handler.go
+++ app/handler/ussd.handler.go
@@ -1,8 +1,6 @@
 package handler
 
 import (
-	"fmt"
-
 	"github.com/Ahmad940/health360/app/service"
 	"github.com/gofiber/fiber/v2"
 )
@@ -12,10 +10,6 @@ func StartUSSD(ctx *fiber.Ctx) error {
 	serviceCode := ctx.FormValue("serviceCode")
 	phoneNumber := ctx.FormValue("phoneNumber")
 	text := ctx.FormValue("text")
-
-	fmt.Println("Session Param 1", ctx.Params("sessionId"), ctx.Params("text"))
-	fmt.Println("Session Query 2", ctx.Query("sessionId"), ctx.Query("text"))
-	fmt.Println("Session GET 3", ctx.Get("sessionId"), ctx.Get("text"))
 
 	res := service.StartUSSD(sessionId, serviceCode, phoneNumber, text)
 
